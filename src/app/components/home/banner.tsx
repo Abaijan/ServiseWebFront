62d5a8ea11b971e6ef17cceeb777776bdcd3a307
Abaijan <fghf09647@gmail.com>
2025-02-02 22:55:53 +0600

    daily commit

--- src/app/components/home/banner.tsx
+++ src/app/components/home/banner.tsx
@@ -1,109 +1,35 @@
-'use client';
 import Image from 'next/image';
-import { Variants } from "framer-motion";
-import { motion } from "framer-motion";
+
 
 export function Banner() {
-
-
     return (
         <section className="w-full">
             <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 items-center h-[800px]">
                 <section>
-                <motion.div variants={
-                    {
-                        hidden: {
-                            opacity: 0,
-                            x: -200,
-                        },
-                        show: {
-                            opacity: 1,
-                            x: 0,
-                        }
-                    }
-                }
-                            transition={{duration: 1.2}}
-                            viewport={{once: true, amount: 0.5}}
-                            initial="hidden"
-                            whileInView="show"
-                            exit="hidden"
-                >
                     <h1 className="text-[50px] font-bold">
                         Welcome to <span className="text-[#f5c312]">FastMart</span>
                     </h1>
-                </motion.div>
-                    <motion.div variants={
-                        {
-                            hidden: {
-                                opacity: 0,
-                                x: -200,
-                            },
-                            show: {
-                                opacity: 1,
-                                x: 0,
-                            }
-                        }
-
-                    }
-                                transition={{duration: 1}}
-                                viewport={{once: true, amount: 0.5}}
-                                initial="hidden"
-                                whileInView="show"
-                                exit="hidden"
-                                className="mt-5"
-                    >
                         <p className="mt-5">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Est et magnam minima pariatur possimus sapiente sed. Architecto expedita harum non?
                         </p>
-                    </motion.div>
                 </section>
                     <section className="w-[40vw] h-[300px] relative">
-                        <motion.div
-                            transition={{duration: 1}}
-                            initial="hidden"
-                            whileInView="show"
-                            exit="hidden"
-                            variants={{
-                                hidden: {
-                                    opacity: 0,
-                                    y: 200,
-                                },
-                                show: {
-                                    opacity: 1,
-                                    y: 0,
-                                },
-                            }}
-                            viewport={{once: true, amount: 0.5}}
-                        >
-                            <Image
-                                className="hover:scale-110 hover:transition hover:ease-out  hover:duration-700 duration-1000 absolute left-[20%]"
-                                src="/images/home/slide1.svg"
-                                alt="Banner slide 1"
-                                width={500}
-                                height={200}
-                            />
-                        </motion.div>
-                        <motion.div transition={{duration: 1.4}} initial="hidden" whileInView="show" exit="hidden"
-                                    variants={{
-                                        hidden: {
-                                            opacity: 0,
-                                            x: 200,
-                                        },
-                                        show: {
-                                            opacity: 1,
-                                            x: 0,
-                                        }
-                                    }} viewport={{once: true, amount: 0.5}}>
 
                             <Image
-                                className="hover:scale-110 hover:transition hover:ease-out hover:duration-700 duration-1000 absolute top-20 z-10"
-                                src="/images/home/slide2.svg"
-                                alt="Banner slide 2"
-                                width={500}
+                                className="hover:scale-110 w-[500px] hover:transition hover:ease-out  hover:duration-700 duration-1000 absolute left-[20%]"
+                                src="/images/home/slide1.svg"
+                                alt="Banner slide 1"
+                                width={1000}
                                 height={200}
                             />
-                        </motion.div>
+                            <Image
+                                className="hover:scale-110 w-[500px] hover:transition hover:ease-out hover:duration-700 duration-1000 absolute top-20 z-10"
+                                src="/images/home/slide2.svg"
+                                alt="Banner slide 2"
+                                width={1000}
+                                height={500}
+                            />
                     </section>
             </section>
         </section>
--- src/app/components/home/flashSale.tsx
+++ src/app/components/home/flashSale.tsx
@@ -1,7 +1,6 @@
-'use client';
-import {motion} from "framer-motion";
+
 import Image from 'next/image';
-import React, {useState} from "react";
+
 interface CardProps {
     id: number;
     category: string;
@@ -9,57 +8,24 @@ interface CardProps {
     title: string;
 }
 
-async function getData() {
-    return await fetch(process.env.NEXT_PUBLIC_SERVER_HOST + '/mainPageCards', { method: 'GET'}).then((res) => res.json())
-}
-export default function FlashSale() {
-    const [cards, setCards] = React.useState<CardProps[]>([]);
-    getData().then((cards) => setCards(cards));
+export default async function FlashSale() {
+    const cards  = await fetch(process.env.SERVER_HOST + '/mainPageCards', {method : 'GET'}).then(res => res.json());
     return (
         <main className="flex h-[100vh]  flex-col items-center justify-center p-24">
-           <motion.div transition={{duration : 1}} variants={
-               {
-                   hidden: {
-                       opacity: 0,
-                       x: -200,
-                   },
-                   show: {
-                       opacity: 1,
-                       x: 0,
-                   }
-               }
-           }>
+
                <div className=" text-center items-center justify-center text-white">
                    <Image src='/images/home/sale.svg' alt='flashSale' width={500} height={200} />
                </div>
                <div className="text-red-500 justify-center items-center mt-[30px] font-extrabold">
                    <h3 className=" ml-10 text-[30px] my-0 mt-auto">Discounts up to 70%! <br/> <span className="text-yellow-700">For next categories:</span>   </h3>
                </div>
-           </motion.div>
             <div className="flex max-w-[1440px] h-[340px] overflow-x-scroll croll-y-none  gap-5 my-[30px]">
                     {
                         cards.map((item: CardProps, index : number) => (
-                <motion.div
-                    key={index} className="z-10 mt-[20px]  hover:scale-110 transition duration-500" transition={{duration: index * 0.2 }} variants={
-                    {
-                        hidden: {
-                            opacity: 0,
-                            y: 300,
-                        },
-                        show: {
-                            opacity: 1,
-                            y: 0,
-                            animation: "spring(1, 80, 10, 0)",
-                        }
-                    }
-                }
-                   initial="hidden" whileInView="show" exit="hidden"
-                >
-                            <div className="bg-cover bg-center w-[250px] h-[300px] relative items-center hovered ">
+                            <div key={index} className="bg-cover bg-center w-[250px] h-[300px] relative items-center hovered ">
                                 <div className="category-backdrop">{item.category}</div>
                                 <Image className="rounded-2xl w-[250px] cursor-pointer h-[300px]   object-center object-cover" width={200} height={300} src={item.image} alt={item.category}/>
                             </div>
-                </motion.div>
                         ))
                     }
             </div>
